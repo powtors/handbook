89621bfccc1ff24b1648a58ac8390268db6dd6ed
Felipe Pitol Puhl <felpofo@proton.me>
2024-07-07 13:31:40 -0300

    feat: header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  type Link = {
+    label: string;
+    href: string;
+  };
+
+  const routes: Link[] = [];
+</script>
+
+<header>
+  <a href="/">
+    <h1>Handbook</h1>
+  </a>
+  <nav>
+    <ul>
+      {#each routes as { label, href }}
+        <li>
+          <a {href} class="contrast">{label}</a>
+        </li>
+      {/each}
+    </ul>
+  </nav>
+</header>
+
+<style lang="scss">
+  header {
+    --pico-typography-spacing-vertical: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    padding:
+      var(--pico-block-spacing-vertical)
+      calc(var(--pico-block-spacing-horizontal) * 1.375);
+
+    border-bottom: 1px solid var(--pico-muted-border-color);
+    margin-bottom: var(--pico-block-spacing-vertical);
+  }
+
+  nav {
+    --pico-nav-element-spacing-vertical: 0;
+  
+    a {
+      text-decoration: none;
+    }
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import "$lib/pico.scss";
+
+  import { Header } from "$lib/components";
+</script>
+
+<Header />
+<slot />
+
+<style lang="scss">
+  :global(html, body) {
+    height: 100%;
+  }
+
+  :global(body) {
+    display: flex;
+    flex-direction: column;
+  }
+
+  :global(body > div > main) {
+    flex: 1;
+  }
+</style>
