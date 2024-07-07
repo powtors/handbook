f4ef6b5fff0b5e07fd6d89ea80bb42e42ae48f1e
Felipe Pitol Puhl <felpofo@proton.me>
2024-07-07 14:57:45 -0300

    chore: error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+</script>
+
+<main>
+  <hgroup>
+    <h1>{$page.status}</h1>
+    {#if $page.error}
+      <p>{$page.error.message}</p>
+    {/if}
+  </hgroup>
+</main>
+
+<style lang="scss">
+  main {
+    display: grid;
+    place-items: center;
+
+    text-align: center;
+  }
+</style>
