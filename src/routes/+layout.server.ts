94f72e03e15e8e7d9ef5f66be84c3201e8d50fcf
Felipe Pitol Puhl <felpofo@proton.me>
2024-10-10 13:23:31 -0300

    chore: use mantainer cache

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,11 +2,15 @@ import type { Account } from "$lib/github";
 import type { LayoutServerLoad } from "./$types";
 import { PUBLIC_MANTAINER_ID } from "$env/static/public";
 
+let mantainer: Account | null = null;
+
 export const load: LayoutServerLoad = async ({ locals, fetch }) => {
   const session = await locals.auth();
 
-  const res = await fetch(`/api/accounts/${PUBLIC_MANTAINER_ID}`);
-  const mantainer: Account = await res.json();
+  if (!mantainer) {
+    const res = await fetch(`/api/accounts/${PUBLIC_MANTAINER_ID}`);
+    mantainer = await res.json();
+  }
 
-  return { session, mantainer };
+  return { session, mantainer: mantainer! };
 };
