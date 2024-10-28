439ca33d86a464fc730062f47fa3a183e6d3bd73
Felipe Pitol Puhl <felpofo@proton.me>
2024-10-27 21:52:20 -0300

    del(dead code): maintainer isn't even used wtf

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,20 +1,7 @@
-import { MAINTAINER_TOKEN } from "$env/static/private";
-
 import type { LayoutServerLoad } from "./$types";
-import type { User } from "$lib";
 
-let maintainer: User;
-
-export const load: LayoutServerLoad = async ({ locals, fetch }) => {
+export const load: LayoutServerLoad = async ({ locals }) => {
   const session = await locals.auth();
 
-  if (!maintainer) {
-    const res = await fetch("https://api.github.com/user", {
-      headers: { authorization: `token ${MAINTAINER_TOKEN}` },
-    });
-
-    maintainer = await res.json().then(({ id, login: name }) => ({ id, name }));
-  }
-
-  return { session, maintainer };
+  return { session };
 };
