4531c7c2d59844afee4d9c2072f7324b3a3b3dd4
Felipe Pitol Puhl <felpofo@proton.me>
2024-11-06 13:14:20 -0300

    fix(post): unhandled 404

--- src/routes/(post)/+layout.server.ts
+++ src/routes/(post)/+layout.server.ts
@@ -4,7 +4,7 @@ import { getPost, renderPost } from "$lib/post";
 export const load: ServerLoad = async ({ params, fetch }) => {
   if (!params.title) throw error(500, "Unreachable #(post)");
 
-  const post = await getPost.bind({ fetch })(params.title).then(renderPost);
+  const post = await getPost.bind({ fetch })(params.title).then(renderPost).catch(() => null);
   if (!post) throw error(404, "Post Not Found");
 
   return { post };
